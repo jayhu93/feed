96b404a523bf693e5d0b2d3749b9aad837ab4dbc
Jay Yupin Hu <jay.yupinhu@gmail.com>
2021-01-08 21:14:43 -0800

    Added empty projects

---
+++ SwiftUIFeed/SwiftUIFeed/SwiftUIFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFeedApp.swift
+//  SwiftUIFeed
+//
+//  Created by Yupin Hu on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
